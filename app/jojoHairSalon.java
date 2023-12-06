9ae1da81ca4aeed2d7a6c294c6852da7a56151bc
monkeymonkey2 <145747651+monkeymonkey2@users.noreply.github.com>
2023-12-05 21:12:23 -0700

    updated
    constructer and stuff

--- app/jojoHairSalon.java
+++ app/jojoHairSalon.java
@@ -1,20 +1,30 @@
 import java.util.ArrayList;
-import Appointment as ap;
+import ap.Appointment;  // Assuming Appointment is in the default package
 
 public class Main {
     public static void main(String[] args) {
-        // Here are the types of appointments:
-        // 0 - Available, 1 = Mens cut $50, 2= Ladies cut $80, 3= Mens Colouring $50, 4= Ladies Colouring $120
-        // create a list of 7 appointments for Saturday (between 9:00 and 15:00 start time)
-        ArrayList<Integer> appt_list = new ArrayList<>();
+        // ArrayList to hold Appointment objects
+        ArrayList<Appointment> appt_list = new ArrayList<>();
         String day = "Saturday";
+
+        // Populate the appt_list with Appointment objects
         for (int time = 9; time <= 15; time++) {
-            appt_list.add(new ap.Appointment(day, time));
+            appt_list.add(new Appointment(day, time));
+        }
+        public class Appointment {
+            private String DayOfTheWeek;
+            private int StartTimeHour;
+            private String ClientName;
+            private String ClientPhone;
+            private int ApptType;
+
+        public Appointment(String day_of_week, int start_time_hour){
+
         }
 
         // Book the first appointment slot (9 AM) for Harvey Wallbanger for a Men's Cut (appt_type = 1)
         int current_appt_index = 0;
-        ap.Appointment current_appt = appt_list.get(current_appt_index);
+        Appointment current_appt = appt_list.get(current_appt_index);
         current_appt.schedule("Harvey", "403-233-3944", 1);
 
         // Book the second appointment slot (10 AM) for Sara for a Ladies Colouring
@@ -27,7 +37,7 @@ public class Main {
         int index = 0;
         while (index < appt_list.size() && !found) {
             current_appt = appt_list.get(index);
-            // is this appointment the noon hour appointment for Saturday available?
+            // Check if this appointment is the noon hour appointment for Saturday and is available
             if (current_appt.get_day_of_week().equals("Saturday")
                     && current_appt.get_start_time_hour() == 12
                     && current_appt.get_appt_type() == 0) {
@@ -35,18 +45,17 @@ public class Main {
             }
             index++;
         }
+
         if (found) {
-            // book it!
-            current_appt.set_client_name("Jenny");
-            current_appt.set_client_phone("403-867-5309");
-            current_appt.set_appt_type(2);  // 2 - Ladies Cut;
+            // Book the noon hour appointment for Jenny
+            current_appt.schedule("Jenny", "403-867-5309", 2);  // 2 - Ladies Cut
         } else {
             System.out.println("Appointment entry not found");
         }
 
         // Print only scheduled appointments using format_record()
         System.out.println("Scheduled appointment records:");
-        for (ap.Appointment appt : appt_list) {
+        for (Appointment appt : appt_list) {
             if (appt.get_appt_type() != 0) {
                 System.out.print(appt.format_record());
             }
@@ -57,9 +66,10 @@ public class Main {
         current_appt = appt_list.get(current_appt_index);
         current_appt.cancel();
 
-        // Print report of all appointment times using string method
-        for (ap.Appointment appt : appt_list) {
-            System.out.print(appt);
+        // Print report of all appointment times using toString() method
+        System.out.println("All appointment records:");
+        for (Appointment appt : appt_list) {
+            System.out.println(appt);
         }
     }
 }
